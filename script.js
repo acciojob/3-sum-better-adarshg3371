fbfeefbc2f9d12d4c0abb7c5efa01c8273ae70b5
Adarsh Gupta <114940577+adarshg3371@users.noreply.github.com>
2023-09-13 11:36:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-function threeSum(arr, target) {
-// write your code here
-  
-}
+function threeSum(nums, target) {
+  nums.sort((a, b) => a - b); // Sort the input array in ascending order.
+  let closestSum = Infinity; // Initialize the closest sum to a large value.
 
-module.exports = threeSum;
+  for (let i = 0; i < nums.length - 2; i++) {
+    let left = i + 1;
+    let right = nums.length - 1;
+
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+
+      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+        closestSum = sum; // Update closest sum if the current sum is closer to the target.
+      }
+
+      if (sum < target) {
+        left++;
+      } else if (sum > target) {
+        right--;
+      } else {
+        return closestSum; // Found an exact match, no need to continue searching.
+      }
+    }
+  }
+
+  return closestSum;
+}
